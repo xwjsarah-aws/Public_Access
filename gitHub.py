69ca1e23f59246afcac540c1d5198526750d5dd5
xwjsarah-aws <55602345+xwjsarah-aws@users.noreply.github.com>
2019-09-20 16:36:03 -0400

    Update gitHub.py

--- gitHub.py
+++ gitHub.py
@@ -2,5 +2,4 @@ from github import Github
 g = Github("132360a81fcc12d6941efed1fc8c793a5ae14dfe")
 for repo in g.get_user().get_repos():
     print(repo.name)
-    #
-    # to see all the available attributes and methodsrepo.edit(has_wiki=False)
+
